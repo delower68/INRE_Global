f9ff257b420be4f243191c88699256466bfa0d15
Delower Hossain <108389033+delower68@users.noreply.github.com>
2023-04-03 17:31:02 +0800

    Navbar & Footer completed

--- src/Pages/Home/Components/RealEstate/RealEstate.js
+++ src/Pages/Home/Components/RealEstate/RealEstate.js
@@ -4,19 +4,6 @@ const RealEstate = () => {
   return (
     <div>
         
-{/* Value	Description	Demo
-clip-source	Defines a URL to an SVG  element	
-basic-shape	Clips an element to a basic shape: circle, ellipse, polygon or inset	
-margin-box	Uses the margin box as the reference box	 
-border-box	Uses the border box as the reference box	 
-padding-box	Uses the padding box as the reference box	 
-content-box	Uses the content box as the reference box	 
-fill-box	Uses the object bounding box as reference box	 
-stroke-box	Uses the stroke bounding box as reference box	 
-view-box	Uses the SVG viewport as reference box	 
-none	This is default. No clipping is done	 
-initial	Sets this property to its default value. Read about initial	
-inherit	Inherits this property from its parent element. Read about inherit
 Value	Description	Demo
 clip-source	Defines a URL to an SVG  element	
 basic-shape	Clips an element to a basic shape: circle, ellipse, polygon or inset	
@@ -42,7 +29,20 @@ stroke-box	Uses the stroke bounding box as reference box
 view-box	Uses the SVG viewport as reference box	 
 none	This is default. No clipping is done	 
 initial	Sets this property to its default value. Read about initial	
-inherit	Inherits this property from its parent element. Read about inherit */}
+inherit	Inherits this property from its parent element. Read about inherit
+Value	Description	Demo
+clip-source	Defines a URL to an SVG  element	
+basic-shape	Clips an element to a basic shape: circle, ellipse, polygon or inset	
+margin-box	Uses the margin box as the reference box	 
+border-box	Uses the border box as the reference box	 
+padding-box	Uses the padding box as the reference box	 
+content-box	Uses the content box as the reference box	 
+fill-box	Uses the object bounding box as reference box	 
+stroke-box	Uses the stroke bounding box as reference box	 
+view-box	Uses the SVG viewport as reference box	 
+none	This is default. No clipping is done	 
+initial	Sets this property to its default value. Read about initial	
+inherit	Inherits this property from its parent element. Read about inherit
     </div>
   )
 }
