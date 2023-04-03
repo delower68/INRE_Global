c2bd30b236f87a39395c9a652b6e3599cba943ef
Delower Hossain <108389033+delower68@users.noreply.github.com>
2023-04-04 01:55:45 +0800

    Real Estate section completed as well

--- src/Pages/Home/Components/RealEstate/RealEstate.js
+++ src/Pages/Home/Components/RealEstate/RealEstate.js
@@ -3,13 +3,14 @@ import  "./RealEstate.css";
 
 const RealEstate = () => {
   return (
-    <div className='flex'>
+    <div className='flex  bg-[#002550] mt-16'>
+        <div className='custom_shap flex  items-center'>
+            <p className='font-[Solway] text-white text-4xl capitalize font-extrabold pl-16'>Making Your <span className='text-[#FAFF00]'>Real Estate</span> purchase <br/> journey faster and transparent</p>
+        </div>
         <div className='bg-cut'>
+          
         </div>
-            <p>Making your real estate purchase journey faster and transparent</p>
-        <div className='bg-img-cut'>
-            <img className='h-[450px]' src='https://i.ibb.co/y4jQnG2/webaliser-TPTXZd9m-Oo-unsplash.jpg'/>
-        </div>
+        
     </div>
   )
 }
