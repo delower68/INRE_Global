8c4b516d287d24e08b5201e18ae0dbf8714aad0f
Delower Hossain <108389033+delower68@users.noreply.github.com>
2023-04-04 03:26:25 +0800

    In contact us section completed 40%

--- src/Pages/Home/Components/ContactUs/ContactUs.js
+++ src/Pages/Home/Components/ContactUs/ContactUs.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import './ContactUs.css'
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+    <div className='p-9 bg-red-200'>
+      <div>
+        <h1 className='text-xl font-semibold text-black font-[Open Sans]'>Get In Touch With Us To Connect</h1>
+        <p>Contact Us</p>
+      </div>
+      <div className='mt-6 flex'>
+        {/* for user full name */}
+        <form className='mr-6'>
+          <div className='input-container'>
+            <input type="text" id="name" autocomplete="off" placeholder="Enter Your name" required className='text-input'/>
+            <label for="name" className='label'>
+              Full Name
+            </label>
+          </div>
+        </form>
+        {/* for user email  */}
+        <form>
+          <div className='input-container'>
+            <input type="text" id="name" autocomplete="off" placeholder="lorem@gmail.com" required className='text-input'/>
+            <label for="name" className='label'>
+              Email Address
+            </label>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
