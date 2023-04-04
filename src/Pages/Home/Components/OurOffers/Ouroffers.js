0eed84aa2340230e20568b9e5751ab2a5f990010
Delower Hossain <108389033+delower68@users.noreply.github.com>
2023-04-04 17:11:51 +0800

    In our offers header  section completed

--- src/Pages/Home/Components/OurOffers/Ouroffers.js
+++ src/Pages/Home/Components/OurOffers/Ouroffers.js
@@ -7,12 +7,20 @@ const Ouroffers = () => {
       <div className='container text-center mt-9 ' >
         <h1 className='text-3xl font-extrabold text-black pb-2 border-b-4 border-[#1A1A1A] mx-96'>Our Offerings</h1>
         <p className='text-base #1A1A1A my-4'>
-          This whole purchase journey can be divided into three stages. For more details, <span className='text-[#002550] font-extrabold'>Click Here</span> 
+          This whole purchase journey can be divided into three stages. For more details, <span className='text-[#002550] font-extrabold'>Click Here</span>
         </p>
       </div>
-      
-      <div>
 
+      <div className=' '>
+        <div className="flex justify-center items-center">
+          <p className="  h-[100px] w-1/3 text-center py-9 bg-[#F2F4F6]"><span className='bg-white p-3.5 rounded-full mr-2'>1</span> Pre-Booking </p>
+          <p className="  h-[100px] w-1/3 py-9 text-center bg-[#CCD3DC] border-b-4 border-[#1A1A1A] ">
+            <span className='bg-white p-3.5 rounded-full mr-2'>2</span>     Post-Booking & Pre-Registration 
+          </p>
+          <p className="  h-[100px] w-1/3 py-9 text-center bg-[#F2F4F6]">
+            <span className='bg-white p-3.5 rounded-full mr-2'>3</span>  Post-Registration 
+          </p>
+        </div>
       </div>
     </div>
   )
