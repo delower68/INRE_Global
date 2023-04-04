0ea7f7a2757c6fc9548b7dfcad23e656c7abe3ea
Delower Hossain <108389033+delower68@users.noreply.github.com>
2023-04-04 19:21:38 +0800

    our team member  section  working

--- src/Pages/Home/Components/OurTeamMember/OurTeamMember.js
+++ src/Pages/Home/Components/OurTeamMember/OurTeamMember.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Team_member_data from "../../../../Fake_Data/Team_member_data"
 
 const OurTeamMember = () => {
   return (
-    <div>OurTeamMember</div>
+    <div>
+      {/* our offers heading  */}
+      <div className='container text-center mt-9 ' >
+        <h1 className='text-3xl font-extrabold text-black pb-2 border-b-4 border-[#1A1A1A] mx-80'>Our Team Members</h1>
+      </div>
+      <div className='grid gap-3 xl:grid-cols-3  h-[560px] rounded-lg my-7 '>
+        {
+          Team_member_data?.Team_member_data?.map(data =>
+            <div className='shadow-2xl	'>
+              <div className='p-5  h-[560px]   rounded-lg'>
+                <img className='w-[380px] h-[280px] mb-7' src={data?.Profile_photo} />
+                <p className='text-2xl font-semibold mb-4'>{data?.Name}</p>
+                <p className='text-justify h-[100px]'>{data?.Details}</p>
+              </div>
+            </div>
+          )
+        }
+      </div>
+    </div>
   )
 }
 
